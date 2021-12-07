22055e5ebf3c64ec8cbc863de87b9ed29c1392ad
someseconds1402 <someseconds1402@gmail.com>
2021-12-07 18:27:38 +0700

    Login & Sign up Function

--- client/client.c
+++ client/client.c
@@ -8,25 +8,34 @@
 #include <string.h>
 #include <signal.h>
 
-#include "handle.h"
 #include "logic.h"
+#include "handle.h"
+
+#include "drawingFunc.h"
+#include "const.h"
 
 #define BUFF_SIZE 255
 
 int sockfd;
 
-void sighandler(int signalType) {
+char sendline[BUFF_SIZE + 1], recvline[BUFF_SIZE + 1];
+
+void sighandler(int signalType)
+{
   char str[] = "quit";
-  if (send(sockfd, str, strlen(str), 0) < 0) {
+  if (send(sockfd, str, strlen(str), 0) < 0)
+  {
     perror("Error");
   };
   close(sockfd);
   exit(1);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-  if (argc != 3 || !isIpV4(argv[1]) || !isNumber(argv[2])) {
+  if (argc != 3 || !isIpV4(argv[1]) || !isNumber(argv[2]))
+  {
     printf("INVALID ERROR.!!!");
     return 0;
   }
@@ -35,10 +44,10 @@ int main(int argc, char *argv[]) {
   char *SERV_ADDR = argv[1];
 
   struct sockaddr_in servaddr;
-  char sendline[BUFF_SIZE+1], recvline[BUFF_SIZE+1];
 
   // create a socket for client
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
     perror("Error");
     exit(2);
   }
@@ -49,7 +58,8 @@ int main(int argc, char *argv[]) {
   servaddr.sin_port = htons(SERV_PORT);
   servaddr.sin_addr.s_addr = inet_addr(SERV_ADDR);
   // connect the client to the server socket
-  if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+  if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+  {
     perror("Problem in connecting to the server");
     return 0;
   }
@@ -57,8 +67,36 @@ int main(int argc, char *argv[]) {
   /* Signal handler specification structure */
   signal(SIGINT, (void (*)(int))sighandler);
   // write code from here
-  char *data = "hello server";
-  send(sockfd, (void*)data, strlen(data), 0);
-	close(sockfd);
+
+  // send(sockfd, (void *)data, strlen(data), 0);
+  // char buffer[BUFF_SIZE];
+  // recv(sockfd, buffer, BUFF_SIZE, 0);
+  // printf("");
+
+  state = 0; // state = MENU;
+  drawMainMenu();
+  while (1)
+  {
+    if (state == MAIN_MENU)
+    {
+      drawMainMenu();
+    }
+    if (state == LOGIN)
+    {
+      drawLoginPage(sockfd);
+    }
+    if (state == SIGN_UP)
+    {
+      drawSignUpPage(sockfd);
+    }
+    if (state == QUIT)
+    {
+      quit();
+      break;
+    }
+  }
+  // printf("Doi hu ao dua em vao con me");
+  // delay(10000);
+  close(sockfd);
   return 0;
 }
--- client/render/handle.c
+++ client/render/handle.c
@@ -10,16 +10,14 @@
 /* divide the string to substrings by strCut
   total: total of substirngs
 */
-char **words(char *line, int *total, char *strCut)
-{
-  char **w = (char **)malloc(sizeof(char) * 100);
-  *w = (char *)malloc(sizeof(char) * 100);
+char **words(char *line, int *total, char *strCut) {
+  char **w = (char**)malloc(sizeof(char)*100);
+  *w = (char*)malloc(sizeof(char)*100);
 
   int n = 0;
-  char *token = strtok(line, strCut);
-  while (token != NULL)
-  {
-    *(w + n++) = token;
+  char * token = strtok(line, strCut);
+  while( token != NULL ) {
+    *(w + n++)  = token;
     token = strtok(NULL, strCut);
   }
   *total = n;
@@ -27,54 +25,45 @@ char **words(char *line, int *total, char *strCut)
 }
 
 /* Check if string converts to number */
-int isNumber(char *str)
-{
+int isNumber(char *str) {
   int i, len = strlen(str);
-  for (i = 0; i < len; i++)
-  {
-    if (str[i] < '0' || str[i] > '9')
-      return 0;
+  for (i = 0; i < len; i++) {
+    if (str[i] < '0' || str[i] > '9') return 0;
   }
   return 1;
 }
 
 /* Check if string is ipv4
 Ex:  127.0.0.1 */
-int isIpV4(char *str)
-{
+int isIpV4(char *str) {
   char temp[100];
   strcpy(temp, str);
   int total;
   char **e = words(temp, &total, ".");
 
-  if (total != 4)
-  {
+  if (total != 4) {
     return 0;
   };
 
   int i;
-  for (i = 0; i < total; i++)
-  {
-    if (!isNumber(e[i]) || atoi(e[i]) < 0 || atoi(e[i]) > 255)
-      return 0;
+  for (i = 0; i < total; i++) {
+    if (!isNumber(e[i]) || atoi(e[i]) < 0 || atoi(e[i]) > 255) return 0;
   }
 
   return 1;
 }
 
-void clearBuffer()
-{
+void clearBuffer() {
   int c;
-  do
-  {
+  do {
     c = getchar();
   } while (c != '\n' && c != EOF);
 }
 
-void addToken(char *str, SignalState signal)
-{
+
+void addToken(char *str, SignalState signal) {
   int len = strlen(str);
   str[len] = '|';
-  str[len + 1] = '0' + signal;
-  str[len + 2] = '\0';
+  str[len+1] = '0' + signal;
+  str[len+2] = '\0';
 }
