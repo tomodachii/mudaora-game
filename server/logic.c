7e074a411cce142689ef34883eae22168cd07015
someseconds1402 <someseconds1402@gmail.com>
2021-12-09 18:34:59 +0700

    Complete login page

--- server/logic.c
+++ server/logic.c
@@ -12,63 +12,93 @@
 #include "linkedList.h"
 #include "logic.h"
 
-void addToken(char *str, SignalState signal) {
+void addToken(char *str, SignalState signal)
+{
   int len = strlen(str);
   str[len] = '|';
-  str[len+1] = '0' + signal;
-  str[len+2] = '\0';
+  if (signal < 10)
+  {
+    str[len + 1] = '0' + signal;
+    str[len + 2] = '\0';
+  }
+  else
+  {
+    str[len + 1] = '0' + signal / 10;
+    str[len + 2] = '0' + signal % 10;
+    str[len + 3] = '\0';
+  }
 }
 
 // login logout signup disconnect
-void answer(int confd, char *message, SignalState signal) {
-  char string[30];
+void answer(int confd, char *message, SignalState signal)
+{
+  char string[50];
   strcpy(string, message);
   addToken(string, signal);
-  while(send(confd, (void*)string, strlen(string), 0) < 0);
+  printf("\n%s - %ld\n", string, strlen(string));
+  while (send(confd, (void *)string, strlen(string), 0) < 0)
+    ;
   // message|signal
   // Login successfully|SUCCESS_SIGNAL
 }
 
-void logIn(User head, int confd, char *username, char *password) {
-  User user = findByName(head, username); 
-  if (user == NULL) {
-    answer(confd, "User is not exist", FAILED_SIGNAL);
-  } else if (user->online > -1) {
-    answer(confd, "Account is login in other client", FAILED_SIGNAL);
-  } else if (strcmp(user->password, password) == 0) {
+void logIn(User head, int confd, char *username, char *password)
+{
+  User user = findByName(head, username);
+  if (user == NULL)
+  {
+    answer(confd, "1", FAILED_SIGNAL); // Tai khoan khong ton tai
+  }
+  else if (user->online > -1)
+  {
+    answer(confd, "2", FAILED_SIGNAL); // Tai khoan da duoc dang nhap boi nguoi khac
+  }
+  else if (strcmp(user->password, password) == 0)
+  {
     user->online = confd; // id of socket connection
-    answer(confd, "Login successfully", SUCCESS_SIGNAL);
-  } else {
-    answer(confd, "Password is wrong", FAILED_SIGNAL);
+    answer(confd, "0", SUCCESS_SIGNAL); // Login thanh cong
+  }
+  else
+  {
+    answer(confd, "3", FAILED_SIGNAL); // Sai mat khau
   }
 }
 
-void signUp(User head, int confd, char *username, char *password) {
+void signUp(User head, int confd, char *username, char *password)
+{
   User user = findByName(head, username);
-  if (user != NULL) {
+  if (user != NULL)
+  {
     answer(confd, "Account is exist", FAILED_SIGNAL);
-  } else {
+  }
+  else
+  {
     answer(confd, "Wellcome", SUCCESS_SIGNAL);
   }
 }
 
-void logOut(User head, int confd) {
+void logOut(User head, int confd)
+{
   User user = head;
-  while(user != NULL) {
-    if (user->online == confd) {
+  while (user != NULL)
+  {
+    if (user->online == confd)
+    {
       user->online = -1;
       break;
-    } 
+    }
     user = user->next;
   }
 }
 
-
 /* go menu and notify the warrior has given up */
-void cancelRound(User head, User player1, User player2) {
+void cancelRound(User head, User player1, User player2)
+{
   User temp = head;
-  while(temp != NULL) {
-    if (temp->online > -1) {
+  while (temp != NULL)
+  {
+    if (temp->online > -1)
+    {
       answer(temp->online, "The warrior has given up", MENU_SIGNAL);
     }
     temp->hp = -1;
@@ -78,35 +108,38 @@ void cancelRound(User head, User player1, User player2) {
   player2 = NULL;
 }
 
-
-void disconnect(User head, User user1, User user2, int confd) {
+void disconnect(User head, User user1, User user2, int confd)
+{
   User user = head;
-  while(user != NULL) {
-    if (user->online == confd) {
+  while (user != NULL)
+  {
+    if (user->online == confd)
+    {
       user->online = -1;
       break;
-    } 
+    }
   }
   close(confd);
 
-  if (user1->online == confd || user2->online == confd) {
+  if (user1->online == confd || user2->online == confd)
+  {
     cancelRound(head, user1, user2);
     return;
   }
-
 }
-
-
 
 // ingame
 
-void gameResult(User head, User winner, User losser) {
+void gameResult(User head, User winner, User losser)
+{
   char *message = "Winner: ";
   strcat(message, winner->username);
 
   User temp = head;
-  while(temp != NULL) {
-    if (temp->online > -1) {
+  while (temp != NULL)
+  {
+    if (temp->online > -1)
+    {
       answer(temp->online, message, MENU_SIGNAL);
       temp->hp = -1;
     }
@@ -116,10 +149,14 @@ void gameResult(User head, User winner, User losser) {
   losser = NULL;
 }
 
-void attack(User head, User attacker, User beingAttacked, int dame) {
-  if (attacker == NULL || beingAttacked != NULL) {
-    if (attacker == NULL) attacker->loss++;
-    if (beingAttacked == NULL) beingAttacked->loss++;
+void attack(User head, User attacker, User beingAttacked, int dame)
+{
+  if (attacker == NULL || beingAttacked != NULL)
+  {
+    if (attacker == NULL)
+      attacker->loss++;
+    if (beingAttacked == NULL)
+      beingAttacked->loss++;
     cancelRound(head, attacker, beingAttacked);
     return;
   }
@@ -129,25 +166,31 @@ void attack(User head, User attacker, User beingAttacked, int dame) {
   // stream(head, attacker, beingAttacked, dame);
 
   beingAttacked->hp -= dame;
-  if (beingAttacked->hp <= 0) {
+  if (beingAttacked->hp <= 0)
+  {
     attacker->win++;
     beingAttacked->loss++;
     gameResult(head, attacker, beingAttacked);
   }
 }
 
-void player(User head, User player1, User player2, int confd) {
+void player(User head, User player1, User player2, int confd)
+{
   User u = head, user = NULL;
-  while(u != NULL) {
-    if (u->online == confd) user = u;
+  while (u != NULL)
+  {
+    if (u->online == confd)
+      user = u;
   }
 
-  if (player1 == NULL) {
+  if (player1 == NULL)
+  {
     player1 = user;
     player1->hp = 1000;
-  } else if (player2 == NULL) {
+  }
+  else if (player2 == NULL)
+  {
     player2 = user;
     player2->hp = 1000;
   }
 }
-
